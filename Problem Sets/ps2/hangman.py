ebb41485c39a3b6ae10c324845311b8e990511ef
GONG <nomlasssssss@outlook.com>
2021-11-25 00:09:55 +0800

    <add>add is_word_guessed()

--- Problem Sets/ps2/hangman.py
+++ Problem Sets/ps2/hangman.py
@@ -60,8 +60,10 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     """
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    for i in range(len(secret_word)):
+        if secret_word[i] not in letters_guessed:
+            return False
+    return True
 
 
 def get_guessed_word(secret_word, letters_guessed):
