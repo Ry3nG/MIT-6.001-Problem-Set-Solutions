781dc000be56ec87dc6c86b48042825c04c0ae05
GONG <nomlasssssss@outlook.com>
2021-11-24 23:44:27 +0800

    <commit> initial commit

---
+++ Problem Sets/ps1/ps1b.py
@@ -0,0 +1,27 @@
+def ps1b():
+    # retrive user input
+    annual_salary = int(input("Enter your annual salary: "))
+    portion_saved = float(
+        input("Enter the percent of your salary to save, as decimal: ")
+    )
+    total_cost = int(input("Enter the cost of your dream home:"))
+    semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal:"))
+
+    # initialize some state variables
+    portion_down_payment = 0.25
+    current_savings = 0
+    r = 0.04
+    monthly_salary = annual_salary / 12
+    month = 0
+    # main body
+    while current_savings < total_cost * portion_down_payment:
+        if month % 6 == 0 and month != 0:
+            monthly_salary *= 1 + semi_annual_raise
+        current_savings += current_savings * r / 12
+        current_savings += monthly_salary * portion_saved
+        month += 1
+
+    print("Number of months:", month)
+
+
+ps1b()
