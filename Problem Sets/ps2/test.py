095a5b9fd580e21bcdbd7b2976c662952926dfb5
GONG <nomlasssssss@outlook.com>
2021-11-25 00:19:48 +0800

    updated test

--- Problem Sets/ps2/test.py
+++ Problem Sets/ps2/test.py
@@ -4,8 +4,15 @@ def is_word_guessed(secret_word, letters_guessed):
             return False
     return True
 
+def get_guessed_word(secret_word, letters_guessed):
+    output = [char for char in secret_word]
+    for i in range(len(secret_word)):
+        if secret_word[i] not in letters_guessed:
+            output[i] ='_'
+    return "".join(output)
+
 
 # test case
 secret_word = "apple"
-letters_guessed = ["i", "e", "l", "p", "p", "a"]
-print(is_word_guessed(secret_word, letters_guessed))
+letters_guessed = ["e",'i','k','p','r', 's']
+print(get_guessed_word(secret_word, letters_guessed))
